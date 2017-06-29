7e4315c18570b86eb3d541ffd67733826ec48b12
Jon Yu <jonathoy@umich.edu>
2017-06-29 11:29:20 -0400

    Add boolean attendance column

--- db/schema.rb
+++ db/schema.rb
@@ -84,6 +84,7 @@ ActiveRecord::Schema.define(version: 20171120163326) do
     t.integer  "position",                           null: false
     t.integer  "top_grades_counted"
     t.boolean  "has_max_points",     default: false, null: false
+    t.boolean  "attendance",         default: false, null: false
   end
 
   create_table "assignments", force: :cascade do |t|
